46607fa9e2590057c773865c1c7110b545821c42
Tim Wicks <Tim Wicks>
2016-05-17 18:34:55 +1000

    patient/resident ui for mood settings

---
+++ wellnet/wellnet/UI/PatientUserHome.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace wellnet.UI
+{
+    public partial class PatientUserHome : ContentPage
+    {
+        public PatientUserHome()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            // send selected mood to database
+        }
+    }
+}
