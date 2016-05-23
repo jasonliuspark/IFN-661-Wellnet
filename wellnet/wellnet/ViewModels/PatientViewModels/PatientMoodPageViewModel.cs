aeab9c599b6519548b901753a6c648715c56b653
Tim Wicks <Tim Wicks>
2016-05-23 10:37:44 +1000

    (Using source tree to commit to Branch 'Tim") Added a grid layout of resident moods for data binding that I am working on.

---
+++ wellnet/wellnet/ViewModels/PatientViewModels/PatientMoodPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+
+namespace wellnet.ViewModels.PatientViewModels
+{
+    public class PatientMoodPageViewModel
+    {
+        public PatientMoodPageViewModel()
+        {
+        }
+    }
+}
---
+++ wellnet/wellnet/Views/PatientViews/PatientMoodPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace wellnet.Views.PatientViews
+{
+    public partial class PatientMoodPage : ContentPage
+    {
+        public PatientMoodPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
